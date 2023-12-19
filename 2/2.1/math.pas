80724190dc2b7c7726ac13f26a98bb1c3bf7c9f6
Yura Ya <yura.ya.dev@gmail.com>
2023-12-19 21:48:05 +0300

    feat: math

---
+++ 2/2.1/math.pas
@@ -0,0 +1,13 @@
+program math;
+begin
+  writeln('9 + 3 = ', 9 + 3, ';');
+  writeln('9 * 3 = ', 9 * 3, ';');
+  writeln('9 - 3 = ', 9 - 3, ';');
+  writeln('9 / 3 = ', 9 / 3, ';');
+  writeln('9 / 3 = ', 9 / 3 : 7 : 3, ';');
+
+  writeln('');
+  writeln('division with reminder: 23 / 4 = (4 * 5) + 3');
+  writeln('23 div 4 = ', 23 div 4, ';');
+  writeln('23 mod 4 = ', 23 mod 4, ';');
+end.
