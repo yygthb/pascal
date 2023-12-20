3677071edc4fec38b16492a971f7a6de12061ea3
Yura Ya <yura.ya.dev@gmail.com>
2023-12-20 15:59:27 +0300

    feat: simple statements sequence

---
+++ 2/2.6/sequence.pas
@@ -0,0 +1,11 @@
+program sequence;
+begin
+  write('Good friend, for Jusus'' sake forbear');
+  readln;
+  write('To dig the dust enclosed here.');
+  readln;
+  write('Blest be the man that spares these stones,');
+  readln;
+  writeln('And curst be he that moves my bones.');
+  writeln('======================')
+end.
